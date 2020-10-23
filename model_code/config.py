df3bee199f8c5f0f16f27507a3cc30e01348ce0a
adeeb10abbas <adeeb.abbas@drexel.edu>
2020-10-23 14:04:30 -0400

    data_loader

--- model_code/config.py
+++ model_code/config.py
@@ -1,18 +1,20 @@
 # some training parameters
 EPOCHS = 10
-BATCH_SIZE = 8
-NUM_CLASSES = 5
+BATCH_SIZE = 64
+NUM_CLASSES = 11
 
 image_height = 128
 image_width = 128
 channels = 3
 
-save_model_dir = "saved_model/model"
-dataset_dir = "dataset/"
+save_model_dir = "../model"
 
-train_dir = dataset_dir + "train"
-valid_dir = dataset_dir + "valid"
-test_dir = dataset_dir + "test"
+dataset_dir = "../data_npy/"
+
+train_img_dir = dataset_dir + "train_img.npy"
+train_lbl_dir = dataset_dir + "train_lbl.npy"
+# valid_dir = dataset_dir + "valid"
+# test_dir = dataset_dir + "test"
 
 # choose a network
 model = "resnet18"
--- model_code/data_loader.py
+++ model_code/data_loader.py
@@ -1,8 +1,10 @@
 import tensorflow as tf 
 import config
+import numpy as np
 
+train_img = np.load(config.train_img_dir)
+train_lbls = np.load(config.train_lbl_dir)
 
-print(config.train_dir)
-
+train_dataset = tf.data.Dataset.from_tensor_slices((train_img, train_lbls))
 
 
