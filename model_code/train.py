8150ab7a6bedb19e3bd55323288c8e304e96a999
Adeeb Abbas <aa4239@drexel.edu>
2020-10-27 23:26:15 -0400

    Update train.py

--- model_code/train.py
+++ model_code/train.py
@@ -26,12 +26,12 @@ def get_model():
 if __name__ == '__main__':
     gpus = tf.config.experimental.list_physical_devices('GPU')
     if gpus:
-	try:
-	    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
-    	    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-    	    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
-  	except RuntimeError as e:
-            print(e)
+		try:
+			tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+			logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+			print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+  		except RuntimeError as e:
+            		print(e)
 
 
     # get the original_dataset
