9f23ef623f48fff07b4268ff5ac1f853afebe1bd
Adeeb Abbas <aa4239@drexel.edu>
2020-11-04 08:53:34 -0500

    Update eval.py

--- eval.py
+++ eval.py
@@ -9,18 +9,18 @@ for mdl in models:
     ##loading the model
     model = tf.keras.models.load_model(mdl)
     encoding_scheme = mdl.split("/")[1].split(".")[0]
-    file = open(encoding_scheme+".csv", "w+")
-    wr = csv.writer(file)
-    for path, i, files in os.walk(f"datasets/{encoding_scheme}"):
-        if "test" in path:
-            if len(files)>0:
-                files = sorted(files)
-                lbl = np.load(path + "/" + files[0])
-                img = np.load(path + "/" + files[1])
-                snr = path.split("/")[3]
-                ## calculating the results for each snr:
-                data = tf.data.Dataset.from_tensor_slices((img, lbl))
-                scores = model.evaluate(data, verbose=1)
-                results_list = [snr, scores[1]]
-                print(f"{encoding_scheme} == {results_list}")
-                wr.writerow(results_list)
+    with open(encoding_scheme+".csv", "w") as wr:
+        wr = csv.writer(file)
+        for path, i, files in os.walk(f"datasets/{encoding_scheme}"):
+            if "test" in path:
+                if len(files)>0:
+                    files = sorted(files)
+                    lbl = np.load(path + "/" + files[0])
+                    img = np.load(path + "/" + files[1])
+                    snr = path.split("/")[3]
+                    ## calculating the results for each snr:
+                    data = tf.data.Dataset.from_tensor_slices((img, lbl))
+                    scores = model.evaluate(data, verbose=1)
+                    results_list = [snr, scores[1]]
+                    print(f"{encoding_scheme} == {results_list}")
+                    wr.writerow(results_list)
