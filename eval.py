4b73713b2449ebb25f7dbed89894cde186b86a60
Adeeb Abbas <aa4239@drexel.edu>
2020-11-03 20:53:11 -0500

    Create eval.py

---
+++ eval.py
@@ -0,0 +1,23 @@
+import tensorflow as tf 
+import os
+import csv
+
+models = ["model/mtf_out_out.h5","model/gadf_out_out.h5", "model/gasf_out_out.h5", "model/out_out_out.h5" ]
+
+for mdl in models:
+    ##loading the model
+    model = tf.keras.models.load_model(mdl)
+    encoding_scheme = mdl.split("/")[2].split(".")[0]
+    file = open(encoding_scheme+".csv")
+    wr = csv.writer(file)
+    for path, i, files in os.walk(f"datasets/{encoding_scheme}"):
+        if "test" in path:
+            files = sorted(files)
+            lbl = np.load(path + "/" + files[0])
+            img = np.load(path + "/" + files[1])
+            snr = path.split("/")[3]
+            ## calculating the results for each snr:
+            data = tf.data.Dataset.from_tensor_slices((img, lbl))
+            scores = model.evaluate(data, verbose=1)
+            results = [snr, scores[1]]
+            wr.writerows(results)
